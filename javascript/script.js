0fdcc51475b01e22593e6e7fb14f0bb9eb23aff4
Bart Erkamp <barterkamp@Barts-MacBook-Pro.local>
2022-12-09 16:17:29 +0100

    add first version mobile navigation

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,23 @@
 "use strict";
 
 const navLink = document.querySelectorAll(".navigation__link");
+const hamburger = document.querySelector(".navigation__hamburger");
+const navMenu = document.querySelector(".navigation__list");
 
 navLink.forEach((link) => {
-
-  console.log(link.href, window.location.href);
   if (link.href === window.location.href) {
     link.setAttribute("aria-current", "page");
   }
+});
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+navLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  });
 });
