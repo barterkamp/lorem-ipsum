401626c59909bcf9a089f2de130dc4b07f162315
Bart Erkamp <barterkamp@Barts-MacBook-Pro.local>
2022-12-09 15:06:30 +0100

    add header section with basic styling

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const navLink = document.querySelectorAll(".navigation__link");
+
+navLink.forEach((link) => {
+
+  console.log(link.href, window.location.href);
+  if (link.href === window.location.href) {
+    link.setAttribute("aria-current", "page");
+  }
+});
