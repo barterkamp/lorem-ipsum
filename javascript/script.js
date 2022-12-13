8eb0ff4ee442de85fef18e4f1c2578f0a180a1f8
Bart Erkamp <barterkamp@Barts-MBP.home>
2022-12-13 15:59:13 +0100

    add lang toggle & first draft gallery section

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,29 @@
 "use strict";
 
+const btnSmall = document.querySelectorAll(".btn--small");
 const navLink = document.querySelectorAll(".navigation__link");
 const hamburger = document.querySelector(".navigation__hamburger");
 const navMenu = document.querySelector(".navigation__list");
+
+// toggle language
+
+// const handleAriaPressed = () => {
+//   btnSmall.getAttribute("aria-pressed") === "true"
+//     ? btnSmall.setAttribute("aria-expanded", "false")
+//     : btnSmall.setAttribute("aria-expanded", "true");
+// };
+
+
+btnSmall.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    btnSmall.forEach((btn) => {
+      btn.classList.remove("active");
+      if (e.target.textContent === btn.textContent) {
+        btn.classList.add("active");
+      }
+    });
+  });
+});
 
 // funtion to set the aria-expanded to true or false when the hamburger menu is toggled.
 const handleAriaExpanded = () => {
