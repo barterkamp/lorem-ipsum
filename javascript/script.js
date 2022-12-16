4d1c45681babc38abad1fb57da05312b321e65c6
Bart Erkamp <barterkamp@Barts-MacBook-Pro.local>
2022-12-16 16:24:21 +0100

    add modal

--- javascript/script.js
+++ javascript/script.js
@@ -4,16 +4,13 @@ const btnSmall = document.querySelectorAll(".btn--small");
 const navLink = document.querySelectorAll(".navigation__link");
 const hamburger = document.querySelector(".navigation__hamburger");
 const navMenu = document.querySelector(".navigation__list");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".modal__overlay");
+const btnCloseModal = document.querySelector(".btn--close");
+const showModal = document.querySelectorAll(".show-modal");
+const body = document.querySelector("body");
 
-// toggle language
-
-// const handleAriaPressed = () => {
-//   btnSmall.getAttribute("aria-pressed") === "true"
-//     ? btnSmall.setAttribute("aria-expanded", "false")
-//     : btnSmall.setAttribute("aria-expanded", "true");
-// };
-
-
+// Toggle active class for language
 btnSmall.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     btnSmall.forEach((btn) => {
@@ -25,32 +22,67 @@ btnSmall.forEach((btn) => {
   });
 });
 
-// funtion to set the aria-expanded to true or false when the hamburger menu is toggled.
-const handleAriaExpanded = () => {
-  hamburger.getAttribute("aria-expanded") === "false"
-    ? hamburger.setAttribute("aria-expanded", "true")
-    : hamburger.setAttribute("aria-expanded", "false");
-};
-
-// loop to set the "aria-current" attribute to show the active link in the navigation
+// Set the "aria-current" attribute to show the active link in the navigation
 navLink.forEach((link) => {
   if (link.href === window.location.href) {
     link.setAttribute("aria-current", "page");
   }
 });
 
-// expands or collapses the hamburger menu
-hamburger.addEventListener("click", () => {
+// Mobile navigation functionality
+
+// Set the aria-expanded to true or false when the hamburger menu is toggled.
+const handleAriaExpanded = () => {
+  hamburger.getAttribute("aria-expanded") === "false"
+    ? hamburger.setAttribute("aria-expanded", "true")
+    : hamburger.setAttribute("aria-expanded", "false");
+};
+
+const toggleMenu = () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
   handleAriaExpanded();
+};
+
+const collapseMenu = () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+  handleAriaExpanded();
+};
+
+hamburger.addEventListener("click", toggleMenu);
+
+navLink.forEach((link) => {
+  link.addEventListener("click", collapseMenu);
 });
 
-// collapses the hamburger menu when a link is clicked
-navLink.forEach((link) => {
-  link.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-    handleAriaExpanded();
-  });
+// Modal functionality
+
+const openModal = (e) => {
+  console.log(e.target);
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+  body.classList.add("modal-open");
+};
+
+const closeModal = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+  body.classList.remove("modal-open");
+};
+
+showModal.forEach((modal) => {
+  modal.addEventListener("click", openModal);
+});
+
+// close modal with close button
+btnCloseModal.addEventListener("click", closeModal);
+
+// close modal by clicking on the overlay
+overlay.addEventListener("click", closeModal);
+
+// close modal by pressing esc key
+document.addEventListener("keydown", (e) => {
+  if (e.key !== "Escape") return;
+  else closeModal();
 });
